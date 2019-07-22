70d81bbe170941f14a9db6b2acb6e47d79ef5b25
Emil Dimitrov <edimitrov1@gmail.com>
2019-07-22 17:15:05 +0300

    Use while on my_each_with_index

--- lib/enumerable_methods.rb
+++ lib/enumerable_methods.rb
@@ -9,9 +9,10 @@ module Enumerable
   end
 
   def my_each_with_index
-    len = self.length - 1
-    for i in 0..len
+    i = 0
+    while i < self.size
       yield(self[i], i)
+      i += 1
     end
     self
   end
